e95b3aa529a2cb8118e8af132fb869455df00732
Ghasem Ramezani <g1999ramezani@gmail.com>
2023-05-24 20:41:36 +0330

    versions thing
    Signed-off-by: Ghasem Ramezani <g1999ramezani@gmail.com>

--- bookkeeper_thing/source/main.cc
+++ bookkeeper_thing/source/main.cc
@@ -2,9 +2,21 @@
 //
 #include <iostream>
 
-int main()
+namespace {
+void PrintHelloMessage()
 {
   using namespace gccore::bookkeeper_thing;
-  
-  std::cout << "Starting: " << kName << "-" << kVersion << "v" << std::endl;
+  std::cout << "[Starting: " << kName << "-" << kVersion << "v]" << std::endl;
+}
+void PrintByMessage()
+{
+  using namespace gccore::bookkeeper_thing;
+  std::cout << "[Closing]: " << kName << "-" << kVersion << "v]" << std::endl;
+}
+}  // namespace
+
+int main()
+{
+  PrintHelloMessage();
+  PrintByMessage();
 }
