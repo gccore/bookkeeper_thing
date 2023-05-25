94d11a0b5669be4f5955ce48c4ec8ffd10437177
Ghasem Ramezani <g1999ramezani@gmail.com>
2023-05-25 21:59:00 +0330

    test
    Signed-off-by: Ghasem Ramezani <g1999ramezani@gmail.com>

--- bookkeeper_thing/source/main.cc
+++ bookkeeper_thing/source/main.cc
@@ -1,22 +1,37 @@
 #include <bookkeeper_thing/version.hh>
 //
+#include <ncurses.h>
+//
 #include <iostream>
+#include <string>
+
+using namespace gccore::bookkeeper_thing;
+
+auto const kTagedName = std::string(kName) + "-" + kVersion;
+auto const kStartMsg = "[STARTING]: " + kTagedName + "v";
+auto const kEndMsg = "[CLOSING]: " + kTagedName + "v";
 
 namespace {
-void PrintHelloMessage()
+void PrintHelloMsg()
 {
   using namespace gccore::bookkeeper_thing;
-  std::cout << "[Starting: " << kName << "-" << kVersion << "v]" << std::endl;
+  ::printw("%s\n", (kStartMsg).c_str());
 }
-void PrintByMessage()
+void PrintBayMsg()
 {
   using namespace gccore::bookkeeper_thing;
-  std::cout << "[Closing]: " << kName << "-" << kVersion << "v]" << std::endl;
+  ::printw("%s\n", (kEndMsg).c_str());
 }
 }  // namespace
 
 int main()
 {
-  PrintHelloMessage();
-  PrintByMessage();
+  initscr();
+  PrintHelloMsg();
+  PrintBayMsg();
+  refresh();
+  getch();
+  endwin();
+
+  return EXIT_SUCCESS;
 }
