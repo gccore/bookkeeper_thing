4e16130ce1977602e5fb272bcc2fd4c13ed88156
Ghasem Ramezani <g1999ramezani@gmail.com>
2023-06-06 19:19:42 +0330

    add consumers list
    Signed-off-by: Ghasem Ramezani <g1999ramezani@gmail.com>

--- bookkeeper_thing/source/widgets/stuff_list.cc
+++ bookkeeper_thing/source/widgets/stuff_list.cc
@@ -26,6 +26,7 @@ void StuffList::draw()
                      structs::Stuff::ST_Personal);
 
   if (radio_btn_idx_ == structs::Stuff::ST_Personal) {
+    ImGui::SeparatorText("Choose the consumers");
     ImGui::Combo(
         "User List", &current_user_idx_,
         [](void* data, int32_t idx, char const** out) -> bool {
@@ -35,11 +36,29 @@ void StuffList::draw()
         },
         (void*)(&const_cast<structs::User::List&>(users_list_)),
         users_list_.size());
+
+    ImGuiTableFlags constexpr kFlags =
+      ImGuiTableFlags_Resizable | ImGuiTableFlags_Reorderable |
+      ImGuiTableFlags_Hideable | ImGuiTableFlags_BordersOuter |
+      ImGuiTableFlags_BordersV;
+    int32_t constexpr kMaxColumn = 1;
+    if (ImGui::BeginTable("Users", kMaxColumn, kFlags)) {
+      ImGui::TableSetupColumn("Name");
+      ImGui::TableHeadersRow();
+
+      for (size_t row_idx = 0; row_idx < tmp_stuff_.consumers.size(); ++row_idx) {
+        ImGui::TableNextRow();
+        ImGui::TableSetColumnIndex(0);
+        ImGui::Text("%s", tmp_stuff_.consumers[row_idx].name.c_str());
+      }
+      ImGui::EndTable();
+    }
+    ImGui::SeparatorText("=");
   }
 
   if (ImGui::Button("Add")) {
     if (!tmp_stuff_.name.empty()) {
-      structs::Stuff stuff;
+      tmp_stuff_.type = structs::Stuff::Type(radio_btn_idx_);
     }
   }
   ImGui::End();
